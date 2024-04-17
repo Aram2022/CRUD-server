3c769fa48c1dc8e6f8da9fef27a9a1e573f154b2
Aram2022 <99878124+Aram2022@users.noreply.github.com>
2024-04-18 00:07:19 +0400

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,22 +4,18 @@ import User from "../models/userModel.js";
 
 const register = async (req, res) => {
   try {
-    // Validate request body
     const { username, email, password } = req.body;
     if (!username || !email || !password) {
       return res.status(400).json({ message: "Missing required fields" });
     }
 
-    // Check if user already exists
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.status(400).json({ message: "User already exists" });
     }
 
-    // Hash the password
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Create new user in the database
     const newUser = new User({
       username,
       email,
@@ -27,7 +23,6 @@ const register = async (req, res) => {
     });
     await newUser.save();
 
-    // Return success message or user data (excluding password) in the response
     res
       .status(201)
       .json({
@@ -42,32 +37,27 @@ const register = async (req, res) => {
 
 const login = async (req, res) => {
   try {
-    // Validate request body
     const { email, password } = req.body;
     if (!email || !password) {
       return res.status(400).json({ message: "Missing email or password" });
     }
 
-    // Find user in the database
     const user = await User.findOne({ email });
     if (!user) {
       return res.status(401).json({ message: "Invalid email or password" });
     }
 
-    // Compare hashed password with provided password
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
       return res.status(401).json({ message: "Invalid email or password" });
     }
 
-    // Generate JWT if passwords match
     const token = jwt.sign(
       { userId: user._id, email: user.email },
       "your-secret-key",
       { expiresIn: "1h" }
     );
 
-    // Return JWT in the response
     res.status(200).json({ message: "Login successful", token });
   } catch (error) {
     console.error("Error logging in:", error);
